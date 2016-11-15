f0cd62384b3b23df07321e1cd457a7622305e2c3
Eduardo André Rodríguez Zelada <eduardo.rodriguezz@outlook.com>
2016-11-14 19:21:49 -0500

    advance first verstion of promoted link menu

--- SHP.PromotedMenu.js
+++ SHP.PromotedMenu.js
@@ -15,6 +15,17 @@
 		return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
 	};
 
+	if (!String.prototype.format) {
+ 		String.prototype.format = function() {
+    	var args = arguments;
+    	return this.replace(/{(\d+)}/g, function(match, number) { 
+      		return typeof args[number] != 'undefined'
+        	? args[number]
+        	: match;
+    		});
+  		};
+	}
+
 	$.fn.PromotedMenu = function(options){
 		var originDiv = this;
 
@@ -47,44 +58,158 @@
 			throw new promotedMenuExeption("menuList", "The List of the Menu is not set");
 		}
 
+		if(settings.menuColor == ''){
+			throw new promotedMenuExeption("menuColor", "The menu must have a color");
+		}
+
 		ExecuteOrDelayUntilScriptLoaded("sp.js", "SP.ClientContext", BuildPrincipalMenu);
 
+		var clientContext = new SP.ClientContext(settings.masterSite);
+		var webSite = clientContext.get_web();
+		var lists = webSite.get_lists();
+		var tileWidth = 160;
+		var tileMargin = 10;
+
 		var principalMenuList;
+		var subMenuList;
+		
 		function BuildPrincipalMenu(){
-			var clientContext = new SP.ClientContext(settings.masterSite);
-			var webSite = clientContext.get_web();
-			var lists = webSite.get_lists();
 			var menuListRef = list.getByTitle(settings.menuList);
 			principalMenuList = menuListRef.getItems('');
 
-			var cols = '';
-			$.each(settins.menuColumns, function(){
-				cols += this + ',';
-			});
-
-			cols = cols.substring(0, cols.length - 1);
-
-			clientContext.load(principalMenuList, "Include(" + cols +")");
+			clientContext.load(principalMenuList, "Include({0})".format(settins.menuColumns.join()));
 			clientContext.executeQueryAsync(PrincipalMenuQuerySucceded, onQueryFailed);
 		}
 
 		function PrincipalMenuQuerySucceded(){
-			var menu = '<ul class="menu" ' + 
-						'style="background-color: ' + settings.menuColor +'">';
+			var menuItem = '';	
 			var listEnumerator = principalMenuList.getEnumerator();
-			
 			while(listEnumerator.moveNext()){
 				var oField = listEnumerator.get_current();
+				var hasSubMenu = oField.get_item(settings.hasSubMenuColumn);
+				var itemClass = hasSubMenu ? 'sub' : 'no-sub';
+				var itemUrl = hasSubMenu ? oField.get_item('url').get_url() : '';
+				menuItem += '<li data-id="{0}" class="{1}" data-url="{2}"><a>{3}</a></li>'.format(oField.get_id(), itemClass, itemUrl, oField.get_item('Title'));
+			}
+			var menu = '<ul class="menu" style="background-color: {0};">{1}</ul>'.format(settings.menuColor, menuItem);
+			$(originDiv).append(menu);
 
-				menu += '<li data-id"' + oField.get_id() + '" '+
-						(!oField.get_item(settings.hasSubMenuColumn)) ?
-						'class="no-sub" data-url="' + oField.get_item(settings.subMenuRedirectColumn).get_url() + '"' 
-						: 'class="sub"') + '>' +
-						'<a ' + 
-						(settings.customImageColumn !== '' ? 
-							'style="background-image:url(\'' + oField.get_item(settings.customImageColumn).get_url() + "')" : '') +
-						'data-img="url(\''
-			}			
+			$('.sub').on('click', function(event){
+				event.preventDefault();
+				if($(this).hasClass('selected'))
+					subMenuListOut(this);
+				else
+					subMenuListIn(this);
+			});
+
+			$('.no-sub').on('click', function(){
+				$(location).attr('href', $(this).attr('data-url'));
+			});
+		}
+
+		function SubMenuListIn(sender){
+			this.menuId = $(sender).attr('data-id');
+			var camlQuery = new SP.CamlQuery();
+			var queryString = 
+			'<View>' + 
+				'<Query>' + 
+				 	'<Where>' +
+						'<Eq>' +
+							'<FieldRef Name=\'MenuSup\' LookupId=\'TRUE\' />' + 
+							'<Value Type=\'Lookup\'>{0}</Value>' +
+						'</Eq>' +
+					'</Where>' +
+					'<OrderBy>' +
+						'<FieldRef Name=\'{1}\' Ascending=\'TRUE\' />' +
+					'</OrderBy>'+
+				'</Query>' +
+			'</View>'
+
+			camlQuery.set_viewXml(queryString.format(this.menuId, settings.orderByColumn));
+			var list = lists.getByTitle(settings.subMenuLibrary);
+			subMenuList = list.getItems(camlQuery);
+			clientContext.load(subMenuList, 'Include({0})'.format(subMenuColumns.join()));
+  			clientContext.executeQueryAsync(onSubMenuCallSucceded, onQueryFailed);
+		}
+
+		function onSubMenuCallSucceded(){
+			if(subMenuList.get_count() == 0) return;
+			$('.menu li').removeClass('selected');
+			$('li[data-id="{0}"]'.format(this.menuId)).addClass('selected');
+			$('.sm').remove();
+     		var hasNotVisible = $(window).width() < ((subMenuList.get_count() * tileWidth) + tileMargin);
+     		var navigationDiv = '';
+     		var listEnumerator = subMenuList.getEnumerator();
+     		if(hasNotVisible){
+     			var back = '{0}/SiteAssets/images_icon/before.png'.format(settings.masterSite);
+     			var forward = '{0}/SiteAssets/images_icon/after.png'.format(settings.masterSite);
+
+     			navigationDiv = 
+     				'<div class="navigation">' +
+     					'<a title="Anterior" class="a">' +
+     						'<span class="sl-link not-active">'+
+     							'<img src="{0}" alt="" class="move"/>'+
+     						'</span>'+
+     					'</a>'+
+     					'<a title="Siguiente" class="s">'+
+     						'<span class="sl-link active">' +
+     							'<img src="{1}" alt="" class="move"/>' +
+     						'</span>' +
+     					'</a>' +
+     				'</div>';
+     			navigationDiv.format(back, forward);
+     		}
+
+     		var tiles = '';
+     		var actual = 0;
+     		while(listEnumerator.moveNext()){
+     			actual++;
+     			var isFirst = actual == 1;
+     			var isLast = actual == subMenuList.get_count();
+
+     			var currentClass = isFirst ? 'first' : isLast ? 'last' : '';
+     			var oField = listEnumerator.get_current();
+     			var url = ofield.get_item('url');
+
+     			var filename = ofield.get_item('FileLeafRef');              
+      			var dir = ofield.get_item('FileDirRef');
+      			filename = dir + '/' + filename;
+
+     			var tile = 
+     				'<div class="c1 {0}">'+
+     					'<div class="c2" style="background-color: {1}">' +
+     						'<a href="{2}" target="_self">' + 
+     							'<img src="{3}" class="img" alt=""/>' +
+     							'<div class="link" offy="100">' +
+     								'<ul><li>{4}</li></ul>' +
+     							'</div>'+
+     						'</a>' +
+     					'</div>' +
+     				'</div>';
+
+     			tiles += tile.format(currentClass, url.get_url(), ofield.get_item(settings.colorColumn), filename, url.get_description())
+     		}
+     		var content = '<div class="submenu">{0}</div><div class="return"><span class="back">Contraer</span></div>'.format(tiles);
+     		var subMenuDiv = '<div class="sm">{0}{1}</div>'.format(navigationDiv, content);
+     		if(hasNotVisible){
+     			$('.s').on('click', advance);
+      			$('.sm').css({'height' : 250, 'overflow-x' : 'hidden' });						  
+      			$('.submenu').css({ 'top' : 190, 'width' : (subMenuList.get_count() * tileWidth) });
+     			$('.return').css({ 'top' : 173 }); 
+     		}
+
+     		$('.back').on('click',subMenuListOut);
+      		$('.sm').slideDown(500);
+      		$('.submenu').slideDown(500);
+      		$('.c2').mouseover(inMouse).mouseout(outMouse);
+		}
+
+
+
+		function subMenuListOut(sender){
+    		$('.submenu').slideUp(500);
+    		$('.sm').slideUp(500);
+    		$('.menu li').removeClass('selected');
 		}
 
 		function onQueryFailed(sender, args) {    
